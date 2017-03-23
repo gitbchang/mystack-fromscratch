d7affef9a678df4afbc69bbd3e16ccedb03148c9
gitbchang <brian.h.chang1@gmail.com>
2017-03-23 18:37:46 -0500

    implemented hot reloading

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -8,6 +8,8 @@ const App = () => <div>
 
 <p>Hey It's me, your Brian</p>
 
+<p>What is React. New Info without reloading the page. Compile Webpack</p>
+
 </div>
 
 export default App
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,11 +2,29 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
 import App from './app'
 
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 
 ReactDOM.render(<App />, document.querySelector(APP_CONTAINER_SELECTOR))
+
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
+
+const wrapApp = AppComponent => <AppContainer>
+                                    <AppComponent />
+                                </AppContainer>
+
+ReactDOM.render(wrapApp(App), rootEl)
+
+if (module.hot) {
+  // flow-disable-next-line
+  module.hot.accept('./app', () => {
+    // eslint-disable-next-line global-require
+    const NextApp = require('./app').default
+    ReactDOM.render(wrapApp(NextApp), rootEl)
+  })
+}
 
 // document.querySelector(APP_CONTAINER_SELECTOR).innerHTML = "<h1>Hello Webpack!</h1> <p>It's me, Brian!</p><p>One more change to see it update</p>"
