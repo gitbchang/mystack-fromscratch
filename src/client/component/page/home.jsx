da2857113df554548630a713d63935f37cd3a29c
gitbchang <brian.h.chang1@gmail.com>
2017-03-30 17:07:25 -0500

    server side rendering implemented. react helmet dynamically adding meta data and page titles

--- src/client/component/page/home.jsx
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import React from 'react'
-
-const HomePage = () => <p>This is the main home page.</p>
-
-export default HomePage
--- src/client/component/page/not-found.jsx
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import React from 'react'
-
-const NotFoundPage = () => <p>404 Page not found</p>
-
-export default NotFoundPage
