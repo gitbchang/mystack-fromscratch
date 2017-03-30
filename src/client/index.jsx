ae15e8a70451ad842b8dc99d8f1a0847ea7b44f3
gitbchang <brian.h.chang1@gmail.com>
2017-03-30 17:23:21 -0500

    socket.io implemented

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -10,6 +10,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { BrowserRouter } from 'react-router-dom'
 
+import setUpSocket from './socket'
 import App from '../shared/app'
 import helloReducer from '../shared/reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
@@ -46,3 +47,5 @@ if (module.hot) {
     ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
+
+setUpSocket(store)
